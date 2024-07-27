b8a1956dcb93c9aa4c05977550c5e9eefade3290
harish3105 <a.harish3105@gmail.com>
2024-07-27 18:57:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
+import transactionsData from './data';
 import './App.css';
 
 function App() {
+  const [transactions, setTransactions] = useState(transactionsData);
+
+  const addTransaction = (newTransaction) => {
+    setTransactions([newTransaction, ...transactions]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<TransactionList transactions={transactions} />} />
+          <Route path="/add" element={<AddTransaction addTransaction={addTransaction} />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
